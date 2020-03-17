a5ca4ed9bc562a8a6f431ef7b407f7e399f4b94a
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-03-17 16:57:08 +0100

    Update Enigmes.h

--- Enigmes.h
+++ Enigmes.h
@@ -6,9 +6,9 @@ typedef struct
  SDL_Rect 	p;
 }Enigme;
 
-void initialiser_enigme(enigme *E);
+void initialiser_enigme(Enigme *E);
 void generer_afficherEnigme (SDL_Surface *ecran  , char image [],Enigme *E,int *RaNd);
 int solution_enigme(char image []);
 void resolution (int *res );
-void afficher_resultat (SDL_Surface *ecran ,int solution,int res,enigme *enigme);
+void afficher_resultat (SDL_Surface *ecran ,int solution,int res,Enigme *enigme);
 #endif // ENIGME_H_INCLUDED
