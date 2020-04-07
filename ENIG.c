7052e6941b318843248a20a999f0f798a1871fe9
youssef994 <63237071+youssef994@users.noreply.github.com>
2020-04-07 22:03:29 +0100

    Update ENIG.c

--- ENIG.c
+++ ENIG.c
@@ -15,17 +15,7 @@ void init_enigme(enigme * e)
 
 }
 
- void generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea)
-{ 
-	int test=*alea ;
-do{
- *alea = 1+ rand()%3;
-}while(*alea==test) ;
- sprintf(image ,"%d.jpg",*alea);
-e->img = IMG_Load(image);
- SDL_BlitSurface(e->img,NULL,screen,&(e->p)) ;
-  SDL_Flip(screen) ;
-}
+ 
  
  int solution_e (char image [])
  {
