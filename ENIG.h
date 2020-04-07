66f4d5a09734915fdc1cd00dd7a5139c835e96e0
youssef994 <63237071+youssef994@users.noreply.github.com>
2020-04-07 23:01:43 +0100

    Update ENIG.h

--- ENIG.h
+++ ENIG.h
@@ -1,39 +1,27 @@
-#ifndef FONCTIONS_H_
-#define FONCTIONS_H_
-
+#ifndef ENIGME_H_INCLUDED
+#define ENIGME_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include<SDL/SDL_ttf.h>
 typedef struct
 {
-SDL_Surface *temps;
-SDL_Rect postemps;
-}temps;
-typedef struct
-{
-SDL_Surface *bouton;
-SDL_Rect posbouton;
-}bouton;
-typedef struct
-{
-SDL_Surface *perso;
-SDL_Rect posperso;
-}perso;
-typedef struct
-{
-SDL_Surface *lab;
-SDL_Rect poslab;
-perso charactere;
-}labyrinthe;
-typedef struct
-{
-SDL_Surface *background;
-SDL_Rect posbackground;
-temps time;
-bouton buttom[2];
+    SDL_Surface *fenetre_enigme;
+    SDL_Surface *image;
+    SDL_Surface *texte;
+    TTF_Font *police;
+    SDL_Rect posim,post;
 }enigme;
 
-void afficherlaby(labyrinthe laby);
-void initialiserEnigme(enigme statique);
-void afficheEnigme(enigme statique);
-void resoudreEnigme(enigme statique);
-void gestionDuTempsEnigme(enigme statique);
+void init_enigme(enigme *enig);
+void remplir_fichier(char fichier_question[]);
+void afficher_enigme(enigme *enig,int *ancienne,char fichier_question[]);
+int solution(int q);
+int resolution(int *affiche);
+void afficher_resultat(enigme *enig,int solution,int resolution);
+void ffree(enigme *enig);
 
-#endif
+
+
+#endif // ENIGME_H_INCLUDED
