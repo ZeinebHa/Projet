090d58ee95aaf0d562b8f2be57d662ba1423211e
youssef994 <63237071+youssef994@users.noreply.github.com>
2020-04-07 22:24:56 +0100

    Update ENIG.h

--- ENIG.h
+++ ENIG.h
@@ -1,15 +1,39 @@
-#include <SDL/SDL.h>
-#include <SDL/SDL_mixer.h>
-#include <SDL/SDL_image.h>
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
 
-typedef struct 
+typedef struct
 {
- SDL_Surface * img;
- SDL_Rect 	p;
+SDL_Surface *temps;
+SDL_Rect postemps;
+}temps;
+typedef struct
+{
+SDL_Surface *bouton;
+SDL_Rect posbouton;
+}bouton;
+typedef struct
+{
+SDL_Surface *perso;
+SDL_Rect posperso;
+}perso;
+typedef struct
+{
+SDL_Surface *lab;
+SDL_Rect poslab;
+perso charactere;
+}labyrinthe;
+typedef struct
+{
+SDL_Surface *background;
+SDL_Rect posbackground;
+temps time;
+bouton buttom[2];
 }enigme;
-
-void init_enigme(enigme * e) ;
-
- int solution_e (char image []) ;
-int resolution (int * running,int *run);
-void afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en) ; 
+void deplacementperso(labyrinthe laby);
+void afficherlaby(labyrinthe laby);
+void initialiserEnigme(enigme statique);
+void afficheEnigme(enigme statique);
+void resoudreEnigme(enigme statique);
+void gestionDuTempsEnigme(enigme statique);
+void generationAleatoirEnigme(enigme dynamique);
+#endif
