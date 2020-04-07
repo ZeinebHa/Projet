d3211e43f980d8caf046fd90c8a4b986bf40cbdc
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-04-07 21:52:02 +0100

    Update Enigmes.c

--- Enigmes.c
+++ Enigmes.c
@@ -11,3 +11,14 @@ void initialiser_enigme(Enigme *E)
 	E->p.y=0;	
 	E->Img=NULL;
 }
+void generate_afficher (SDL_Surface *ecran  , char image [],enigme *E,int *RaNd)
+{ 
+	int test=*RaNd ;
+	srand(time(NULL));
+ *RaNd = 1+ rand()%3;
+
+ sprintf(image ,"%d.jpg",*RaNd);
+E->img = IMG_Load(image);
+ SDL_BlitSurface(E->img,NULL,ecran,&(E->p)) ;
+  SDL_Flip(screen) ;
+}
