37197986d1208ea4010dd1344db29e2f2212d152
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-04-07 22:59:12 +0100

    Update Enigmes.c

--- Enigmes.c
+++ Enigmes.c
@@ -22,3 +22,62 @@ E->img = IMG_Load(image);
  SDL_BlitSurface(E->img,NULL,ecran,&(E->p)) ;
   SDL_Flip(screen) ;
 }
+
+int solution_enigme(char image [])
+{
+   	int solution =0 ;
+ 	
+ 	if(strcmp(image,"1.jpg")==0)
+ 	{
+     solution =1 ;
+ 	}
+ 	else  	if(strcmp(image,"2.jpg")==0)
+ 	{
+ 		solution =2;
+
+ 	}
+ 	else 	if(strcmp(image,"3.jpg")==0)
+ 	{
+ 		solution =3;	
+ 	}
+ 	return solution;
+}
+void resolution (int *res )
+{
+    	SDL_Event event ;
+  	int continuer=1;
+	
+	while(continuer)
+	{
+		SDL_WaitEvent(&event);
+	            switch(event.type)
+	{
+		  case SDL_QUIT:
+		     continuer=0;
+		 reak ;
+
+                  case SDL_KEYDOWN : 
+                         switch( event.key.keysym.sym )
+                          {
+			  case  SDLK_a: 
+					 
+			  *res= 1;
+				continuer=0;
+			   break ;
+					 
+			   case  SDLK_b:
+					 
+			   *res= 2;
+			     continuer=0;
+			   break;
+					 
+			   case SDLK_e: 
+					 
+			   *res=3 ;
+			continuer=0;
+			  break;
+					 
+			    }
+       break ;
+
+}
