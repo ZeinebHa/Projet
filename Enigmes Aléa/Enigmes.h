fbca01e84e1f3e1476a6f4c6e42908c862b9efd7
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-04-11 20:57:35 +0100

    Update Enigmes.h

--- Enigmes Aléa/Enigmes.h
+++ Enigmes Aléa/Enigmes.h
@@ -6,9 +6,26 @@ typedef struct
  SDL_Rect 	p;
 }Enigme;
 
+typedef struct
+{
+	char  R_True[10];
+	char quest[100];
+	char A1[10];
+	char A2[10];
+}Question;
+
+typedef struct 
+{
+	Question e_q[30];
+	int reponse;
+}Answer;
+
+
 void initialiser_enigme(Enigme *E);
-void generer_afficherEnigme (SDL_Surface *ecran  , char image [],Enigme *E,int *RaNd);
-int solution_enigme(char image []);
-void resolution (int *res );
+void generer(SDL_Surface *ecran, char image [],Enigme *E ,int *Rand);
+void afficher(enigme *E ,int val);
+int solution_enigme(char image [],int *reponse);
+
 void afficher_resultat (SDL_Surface *ecran ,int solution,int res,Enigme *enigme);
+void liberer ();
 #endif // ENIGME_H_INCLUDED
