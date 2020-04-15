e1f0ef9e019d6b222b7608e3356732bf4cbd350a
Ahmedbenabdallah29435 <63337471+Ahmedbenabdallah29435@users.noreply.github.com>
2020-04-15 10:37:35 +0100

    Update zombie.h

--- Tache 2_Benabdallah Ahmed/zombie.h
+++ Tache 2_Benabdallah Ahmed/zombie.h
@@ -21,12 +21,22 @@ SDL_Rect ghostpos;
 int i;	
 }ghost;
 
+
+
+
+
 void initzombie(zombie* z);
-void initbat(bat* b);
-void movezombie(zombie* z,SDL_Surface* screen);
+void initzombie2(zombie* z);
 void changeposzombie(zombie* z);
+void movezombie(zombie* z,SDL_Surface* screen);
+
 void movezombie2(zombie* z,SDL_Surface* screen);
+void initbat(bat* b);
 void affichezbat(bat* b,SDL_Surface *screen);
+void initghost(ghost* g);
+void initghost2(ghost* g);
+
+void affichezghost(ghost* g,SDL_Surface* screen,int vision,personnage *p);
 
 
 #endif 
