d3378749fc2831009f94f40a3568c6d63c8aaa1b
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-05-31 11:03:52 +0100

    Create Sauvegarder.h

---
+++ Enigmes Aléa/Tache_Save&Update_Menu/Sauvegarder.h
@@ -0,0 +1,16 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+typedef struct save//toutes les structures+collision?
+{
+	personnage p; 
+	zombie z;
+  SDL_Surface *background;
+	FILE *fichier;
+}save; 
+
+void update_save (save *s, personnage p,zombie z,);
+void enregistrer (save s);
+void recuperer (save *s);
+
+#endif
