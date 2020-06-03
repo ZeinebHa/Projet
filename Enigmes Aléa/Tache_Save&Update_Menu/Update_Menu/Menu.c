c1124d69beb811b2b8a56626285b1c449798356b
Zeineb Haraketi <61319733+ZeinebHa@users.noreply.github.com>
2020-06-03 11:48:35 +0100

    Create Menu.c

---
+++ Enigmes Aléa/Tache_Save&Update_Menu/Update_Menu/Menu.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include "Menu.h"
+
+void musique (Mix_Music *music)
+{
+	if (Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,MIX_DEFAULT_CHANNELS,1024)==-1)
+	{
+		printf("%s",Mix_GetError());
+	}
+	music=Mix_LoadMUS("music.mp3");
+	Mix_PlayMusic(music,-1);
+}
