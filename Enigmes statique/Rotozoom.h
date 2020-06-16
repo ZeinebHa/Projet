578b08441700e616022474d991493eb1c1c25bd6
youssef994 <63237071+youssef994@users.noreply.github.com>
2020-06-16 19:07:15 +0100

    Delete Rotozoom.h

--- Enigmes statique/Rotozoom.h
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-
-void zoom(int *tempsActuel,int *tempsPrecedent,SDL_Surface *ecran,double *angle,SDL_Surface *image,SDL_Surface *rotation,SDL_Rect rect,int TEMPS);
