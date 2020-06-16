fa376b9796bb92ace930d2b611e650125fc57105
youssef994 <63237071+youssef994@users.noreply.github.com>
2020-06-16 23:47:14 +0100

    Add files via upload

---
+++ tache 5_youssef ben slama/rotozoom/main.c
@@ -0,0 +1,32 @@
+
+
+/**
+* @file main.c
+* @brief Testing Program.
+* @author youssef ben slama
+* @version 0.1
+* @date june 14 2020
+*
+* Testing program for rotozoom
+*
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
+#include "rotozoom.h"
+
+
+int main(int argc, char *argv[])
+{
+
+ rotozoom();
+
+
+ SDL_Quit();
+
+return 0;
+}
+
