8b04e86bf31b9c6788c8d24da6c3a3edbd1c10e2
Ahmedbenabdallah29435 <63337471+Ahmedbenabdallah29435@users.noreply.github.com>
2020-06-17 08:17:51 +0100

    Add files via upload

---
+++ Tache 2:AHMED BEN ABDALLAH/proto1/fonction.h
@@ -0,0 +1,54 @@
+#ifndef _FONCTIONS_H_
+#define _FONCTIONS_H_
+/**
+* @author ahmed ben abdallah
+*/
+
+
+/**
+* @struct ennemi
+* @brief struct for ennemi
+*/
+
+typedef struct ennemi
+{
+
+SDL_Surface *sprite ;
+
+
+SDL_Rect posSprite[2][6];
+
+int ligne ;
+int col ;
+
+SDL_Rect positionennemi ;
+
+int direction ;
+
+
+
+}ennemi;
+  
+               
+
+
+
+ennemi initialisation_ennemi(ennemi e);
+
+void afficher(ennemi e ,SDL_Surface *ecran) ;
+
+void deplacement_aleatoire(ennemi *e);
+
+void animer_ennemi(ennemi *e ) ;
+
+void maj_ennemi (ennemi *e) ;
+
+int collision_bb(ennemi e , SDL_Rect poshero ) ;
+
+
+
+
+
+
+#endif
+
